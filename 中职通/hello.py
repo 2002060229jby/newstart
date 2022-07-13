2905f48d30d1662ec5c3f8ebc78d2723d10aac81
adam <2690528565@qq.com>
2022-07-13 11:10:07 +0800

    dsa

--- 中职通/hello.py
+++ 中职通/hello.py
@@ -1,4 +1,29 @@
-import time 
-print('我是一个接待程序')
-time.sleep(3)
-print('请跟我来')
+# 打开command palette 输入：select Interpreter 选择解释器
+# -----------------------------------------------
+# import time
+# print('我是一个接待程序')
+# time.sleep(1)
+# print('请跟我来')
+#
+# print('dsad'
+#       'dasdsa'
+#       'dasdfsfds')
+#
+# passwd = input('请输入密码：')
+# print(passwd)
+# print(type(passwd))
+
+'''
+ctrl + / 快速注释
+'''
+
+'''
+命名习惯：
+- 见名知意
+- 大驼峰、小驼峰、下划线
+- 不能以数字开头
+'''
+
+
+d=(1,2,3)
+print(type(d))
